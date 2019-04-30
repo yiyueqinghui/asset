a176acb4b70a91e03f5938b990f6d3598ffe1f49
chenjun6 <chenjun6.com>
2019-04-30 10:10:43 +0800

    做了兼容ie9的处理

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'babel-polyfill'      //兼容ie9处理
+import 'es6-promise/auto'
 import axios from './api/index'
 Vue.prototype.$axios = axios
 import Store from './store/store'
 Vue.prototype.$Store = Store
+
+
 
 Vue.config.productionTip = false
 
