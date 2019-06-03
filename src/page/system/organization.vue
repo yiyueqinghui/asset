370899fe2d6304673f6d35bad90ff4a58dbd04d6
chenjun6 <chenjun6.com>
2019-06-03 16:53:52 +0800

    新增系统管理的组织结构、角色管理页面

--- src/page/system/organization.vue
+++ src/page/system/organization.vue
@@ -7,12 +7,13 @@
              :data="data"
              node-key="id"
              highlight-current
-             :current-node-key="currentKey">
+             :current-node-key="currentKey"
+             @node-click="checkedNode">
            </el-tree>
          </el-col>
          <el-col  :span="18" :sm="18" class="detail">
            <el-row >
-             <el-dropdown trigger="hover" style="margin-left: 10px;" @command="addBtn">
+             <el-dropdown trigger="hover" style="margin-right: 10px;" @command="addBtn">
                <el-button type="primary">
                   新建
                </el-button>
@@ -23,11 +24,11 @@
                  <el-dropdown-item  command="4">新建下级部门</el-dropdown-item>
                </el-dropdown-menu>
              </el-dropdown>
-             <el-button class="btns"  type="primary" icon="el-icon-edit">修改</el-button>
-             <el-button class="btns" type="primary" icon="el-icon-edit"  >删除</el-button>
+             <el-button   type="primary" @click="changeTree" icon="el-icon-edit">修改</el-button>
+             <el-button  type="primary" @click="deleteTree" icon="el-icon-edit"  >删除</el-button>
            </el-row>
            <el-row class="header">
-              <i class="org_icon"></i>佳禾集团/集团总部/管理层
+              <i class="org_icon"></i>{{treeNav}}
            </el-row>
            <el-row class="headerInfo">
              <i class="el-icon-s-home"></i> 基本信息
@@ -46,32 +47,50 @@
        </el-row>
 
       <el-dialog :title="dialogTitle" width="960px" :visible.sync="dialogFormVisible">
-        <el-form :model="formData" label-width="auto"  class="demo-form-inline self-input border">
+        <el-form :model="formData" label-width="auto" v-if="dialogFormVisible"  class="demo-form-inline self-input border">
           <el-row>
               <el-col :sm="12">
                 <SelfInput  :labelName="this.typeName+'编码'"   keyName="code" :val="formData.code"  @changeFormVal="changeFormVal"></SelfInput>
               </el-col>
               <el-col :sm="12">
-                <SelfInput  :labelName="this.typeName+'全称'"   keyName="name" :val="formData.label"  @changeFormVal="changeFormVal"></SelfInput>
+                <SelfInput  :labelName="this.typeName+'全称'"   keyName="label" :val="formData.label"  @changeFormVal="changeFormVal"></SelfInput>
               </el-col>
           </el-row>
           <el-row v-if="this.createType >= 3">
-            <el-col :sm="12">
+            <el-col :sm="12" v-if="formData.blongCompany">
               <SelfInput  labelName="所属公司"   keyName="blongCompany" :val="formData.blongCompany"  @changeFormVal="changeFormVal" :disabled="true"></SelfInput>
             </el-col>
-            <el-col :sm="12">
+            <el-col :sm="12" v-if="formData.blongDepart">
               <SelfInput  labelName="上级部门"   keyName="blongDepart" :val="formData.blongDepart"  @changeFormVal="changeFormVal" :disabled="true"></SelfInput>
             </el-col>
           </el-row>
           <el-row  v-else>
-            <el-col :sm="12" >
+            <el-col :sm="12" v-if="formData.blongCompany" >
               <SelfInput  labelName="上级公司"   keyName="blongCompany" :val="formData.blongCompany"  @changeFormVal="changeFormVal" :disabled="true"></SelfInput>
             </el-col>
           </el-row>
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+          <el-button type="primary" @click="confirm">确 定</el-button>
+        </div>
+      </el-dialog>
+
+      <!--修改-->
+      <el-dialog title="修改" width="960px" :visible.sync="visible">
+        <el-form :model="form" label-width="auto" v-if="visible"  class="demo-form-inline self-input border">
+          <el-row>
+            <el-col :sm="12">
+              <SelfInput  :labelName="this.changeName+'编码'"   keyName="code" :val="form.code"  @changeFormVal="changeFormVal"></SelfInput>
+            </el-col>
+            <el-col :sm="12">
+              <SelfInput  :labelName="this.changeName+'全称'"   keyName="label" :val="form.label"  @changeFormVal="changeFormVal"></SelfInput>
+            </el-col>
+          </el-row>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="visible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmChange">确 定</el-button>
         </div>
       </el-dialog>
 
@@ -94,27 +113,30 @@
                type:'1',
                children: [{
                  id: 4,
-                 parentId:1,
                  label: '二级部门 1-1',
                  code:'002',
                  type:'2',
                  children: [{
                    id: 9,
-                   parentId:4,
                    label: '三级部门 1-1-1',
                    code:'003',
                    type:'2',
                  }, {
                    id: 10,
-                   parentId:4,
                    label: '三级部门 1-1-2',
                    code:'004',
                    type:'2',
                  }]
                }]
              }
+
            ],
+           treeNav:'',
+           treeDetail:{
+
+           },
            currentKey:0,
+           firstLevelId:[],
            basicInfo:{
              code:'001',
              name:'管理层',
@@ -130,6 +152,11 @@
               blongCompany:'',
               blongDepart:''
            },
+           form:{
+             code:'',
+             label:''
+           },
+           visible:false,
            createType:0,
            dialogFormVisible:false
 
@@ -137,18 +164,50 @@
       },
       methods:{
         addBtn(type){
-          this.createType = type;
-          this.formData.type = this.createType >=3?'1':'2';
-          let currentKey = this.$refs.tree.getCurrentKey();
+          this.resetData();
+          let firstLevelId = [];
+          this.data.forEach(item=>{
+            firstLevelId.push(item.id);
+          })
+          this.firstLevelId = firstLevelId;
 
-          let parentData = this.getParentData(currentKey);
-          console.log(parentData);
-          this.formData.blongCompany = parentData.blongCompany;
-          this.formData.blongDepart = parentData.blongDepart;
+          this.createType = type;
+          this.formData.type = this.createType >=3?'2':'1';
+          let currentKey = this.$refs.tree.getCurrentKey();
+          console.log('当前节点',currentKey);
+          this.currentKey = currentKey;
+
+          if(currentKey){
+            let currentData = this.$refs.tree.getCurrentNode();
+            if(this.firstLevelId.indexOf(currentKey) == -1){
+              let parentData = this.getParentData(currentKey);
+              this.formData.blongCompany = parentData.blongCompany;
+              this.formData.blongDepart = parentData.blongDepart;
+            }else{
+              if(this.createType == 2 || this.createType ==4){
+                this.formData.blongCompany = this.$refs.tree.getCurrentNode().label;
+              }
+            }
+          }else{
+            this.currentKey = '';
+            if(this.data.length != 0){
+              this.$message({
+                message: '请先选择一个公司或部门！',
+                type: 'warning'
+              });
+              return;
+            }
+          }
 
           console.log(this.formData);
-
           this.dialogFormVisible = true;
+        },
+        resetData(){
+          let data = Object.assign({},this.formData);
+          Object.keys(data).forEach(item=>{
+            data[item] = '';
+          })
+          this.formData = data;
         },
         getParentData(id){
           let that = this;
@@ -156,14 +215,24 @@
             blongCompany:'',
             blongDepart:''
           };
-          search(id);
+          if(this.createType == 1 || this.createType == 3){
+            search(id);      //新建同级
+          }else{
+            let data = this.$refs.tree.getCurrentNode();
+            if(data.type == '1'){
+              uperInfo.blongCompany = data.label;
+            }else{
+              search(id);      //新建下级
+              uperInfo.blongDepart = data.label;
+            }
+
+          }
+          console.log(uperInfo);
+
           return uperInfo;
 
           function search(currentId){
             let parentData = that.$refs.tree.getNode(currentId).parent.data;
-            console.log(parentData);
-            let level = parentData.$treeNodeId;
-            console.log(level);
             var currentId = parentData.id;
             if(parentData.type === '1'){
               uperInfo.blongCompany = parentData.label
@@ -173,7 +242,7 @@
 
             if(uperInfo.blongCompany){
               return
-            }else if(level == 1){
+            }else if(that.firstLevelId.indexOf(currentId) >=0){
               return
             }else{
               search(currentId);
@@ -182,10 +251,78 @@
 
 
         },
-
         changeFormVal([key,val]){
-          console.log(val);
           this.formData[key] = val;
+        },
+        confirm(){
+          let data = {
+            id:parseInt(Math.random()*1000),
+            code:this.formData.code,
+            label: this.formData.label,
+            type:this.formData.type
+          }
+          console.log(this.currentKey);
+          if(this.currentKey === ''){
+            this.data.push(data);
+            this.dialogFormVisible = false;
+            return;
+          }
+
+          if(this.createType == '2' || this.createType == '4'){
+            console.log('下级')
+            this.$refs.tree.append(data,this.currentKey);
+          }else{
+            console.log('同级',this.currentKey)
+            this.$refs.tree.insertAfter(data,this.currentKey);
+          }
+
+          console.log(this.data);
+          this.dialogFormVisible = false;
+        },
+        changeTree(){
+          let checkedData = this.$refs.tree.getCurrentNode();
+          if(!checkedData){
+            this.$message({
+              message: '请先选择一个公司或部门！',
+              type: 'warning'
+            });
+            return;
+          }
+          this.form = Object.assign({},checkedData)
+          this.visible = true;
+        },
+        confirmChange(){
+          this.visible = false;
+        },
+        deleteTree(){
+          let checkedData = this.$refs.tree.getCurrentNode();
+          if(!checkedData){
+            this.$message({
+              message: '请先选择一个公司或部门！',
+              type: 'warning'
+            });
+            return;
+          }else{
+            this.$message({
+              message: '删除成功！',
+              type: 'success'
+            });
+          }
+        },
+        checkedNode(data){
+          this.treeNav = '';
+          let firstLev = [];
+          this.data.forEach(item=>{
+            firstLev.push(item.id)
+          })
+          let arr = [];
+          while (data){
+            arr.unshift(data.label);
+            if(firstLev.indexOf(data.id)>=0)  break;
+            data = this.$refs.tree.getNode(data.id).parent.data;
+          }
+          this.treeNav = arr.join('/');
+
         }
       },
       computed:{
@@ -197,6 +334,13 @@
             name = '公司'
           }
           return name;
+        },
+        changeName:function(){
+          if(this.type == 1){
+            return '公司'
+          }else{
+            return '部门'
+          }
         },
         dialogTitle:function(){
           let type = this.createType;
--- src/page/system/roles.vue
+++ src/page/system/roles.vue
@@ -1,15 +1,468 @@
 <template>
     <div id="roles">
-          角色
+      <el-row>
+        <el-button  type="primary" icon="el-icon-plus" @click="editRole(1)">新建角色</el-button>
+        <el-button  type="primary" icon="el-icon-edit"  @click="editRole(2)">修改</el-button>
+        <el-button  type="primary" icon="el-icon-edit"  @click="editRole(3)">删除</el-button>
+        <el-button  type="primary" icon="el-icon-plus" @click="authRole">角色授权</el-button>
+        <el-button  type="primary" icon="el-icon-s-custom" @click="assignUser" >分配用户</el-button>
+      </el-row>
+      <el-row>
+        <el-table :data="roleData" class="roleTab"  @selection-change="handleSelectionChange" ref="multipleTable"  border stripe fit stripe style="overflow-x: auto">
+          <el-table-column type="selection" width="55">
+          </el-table-column>
+          <el-table-column type="index" label="序号" width="60" align="center">
+          </el-table-column>
+          <el-table-column  label="角色名称" class="roleName" prop="roleName"  align="center">
+          </el-table-column>
+          <el-table-column  label="备注" prop="remarks"  align="center">
+          </el-table-column>
+        </el-table>
+      </el-row>
+      <!--新建角色-->
+      <el-dialog
+        :close-on-click-modal="false" :close-on-press-escape="false"
+        title="新建角色"
+        :visible.sync="createVisible"
+        width="560px"
+        top="80px">
+        <el-form :model="createForm"  label-width="auto"  class="demo-form-inline self-input">
+          <el-form-item label="角色名称">
+            <el-input v-model="createForm.roleName"></el-input>
+          </el-form-item>
+          <el-form-item label="备注">
+            <el-input type="textarea" :rows="3"  v-model="createForm.remarks"></el-input>
+          </el-form-item>
+        </el-form>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="createVisible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmCreate">确 定</el-button>
+        </div>
+      </el-dialog>
+      <!--编辑角色-->
+      <el-dialog
+        class="editDialog"
+        :close-on-click-modal="false" :close-on-press-escape="false"
+        title="修改角色"
+        :visible.sync="editVisible"
+        width="560px"
+        top="80px">
+        <el-menu default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">角色信息</el-menu-item>
+          <el-menu-item index="2">角色权限</el-menu-item>
+          <el-menu-item index="3">成员列表</el-menu-item>
+        </el-menu>
+        <!--角色信息-->
+        <el-form v-if="currentIndex === 1" :model="createForm"  label-width="auto"  class="demo-form-inline self-input">
+          <el-form-item label="角色名称">
+            <el-input v-model="createForm.roleName"></el-input>
+          </el-form-item>
+          <el-form-item label="备注">
+            <el-input type="textarea" :rows="3"  v-model="createForm.remarks"></el-input>
+          </el-form-item>
+        </el-form>
+        <!--角色权限-->
+        <el-row v-if="currentIndex === 2">
+          <el-tree
+            highlight-current
+            :data="roleAuth"
+            show-checkbox
+            node-key="id"
+            :default-expanded-keys="expandedKeys"
+            :default-checked-keys="checkedKeys"
+            :current-node-key="currentKey"
+            @check="checkedChange"
+            :props="defaultProps">
+          </el-tree>
+        </el-row>
+        <!--成员列表-->
+        <el-row v-if="currentIndex === 3">
+          <el-row class="personZone">
+            <el-button class="btns"  type="primary" icon="el-icon-plus" @click="addPerson">添加人员</el-button>
+            <el-button class="btns" type="primary" icon="el-icon-edit"  @click="deletePerson">删除</el-button>
+            <el-input v-model="searchVal"  suffix-icon="el-icon-search" @blur="searchPerson" class="search"  placeholder="请输入内容进行查寻"/>
+          </el-row>
+          <el-scrollbar class="dialogZone">
+            <el-table :data="personData" class="personTab"  @selection-change="handleSelection"  border stripe fit stripe style="overflow-x: auto">
+              <el-table-column type="selection" width="55">
+              </el-table-column>
+              <el-table-column type="index" label="序号" width="60" align="center">
+              </el-table-column>
+              <el-table-column  label="姓名" prop="name"  align="center">
+              </el-table-column>
+              <el-table-column  label="所在机构" prop="blongDepart"  align="center">
+              </el-table-column>
+              <el-table-column  label="职位名称" prop="employee"  align="center">
+              </el-table-column>
+            </el-table>
+          </el-scrollbar>
+        </el-row>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="editVisible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmEdit">确 定</el-button>
+        </div>
+      </el-dialog>
+      <!--角色授权-->
+      <el-dialog
+        :close-on-click-modal="false" :close-on-press-escape="false"
+        title="角色授权"
+        :visible.sync="authVisible"
+        width="560px"
+        top="80px">
+        <el-form :model="authForm"  label-width="auto"  class="demo-form-inline self-input">
+          <el-form-item label="角色名称">
+            <el-input disabled="disabled" v-model="authForm.roleName"></el-input>
+          </el-form-item>
+          <el-form-item label="授权">
+            <el-tree
+              highlight-current
+              :data="roleAuth"
+              show-checkbox
+              node-key="id"
+              :default-expanded-keys="expandedKeys"
+              :default-checked-keys="checkedKeys"
+              :current-node-key="currentKey"
+              @check="checkedChange"
+              :props="defaultProps">
+            </el-tree>
+          </el-form-item>
+        </el-form>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="authVisible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmAuth">确 定</el-button>
+        </div>
+      </el-dialog>
+      <!--分配用户-->
+      <el-dialog
+        :close-on-click-modal="false" :close-on-press-escape="false"
+        title="分配用户"
+        :visible.sync="userVisible"
+        width="560px"
+        top="80px">
+        <el-form :model="userForm"  label-width="auto"  class="demo-form-inline assign">
+          <el-form-item label="角色名称">
+            <el-input disabled="disabled" v-model="userForm.roleName"></el-input>
+          </el-form-item>
+          <el-form-item label="用户">
+            <el-cascader
+              :value="userForm.userList"
+              :options="options"
+              :props="propsData"
+              :show-all-levels="false"
+              filterable
+              clearable>
+              <template slot-scope="{ node, data }">
+                <span>{{ data.label }}</span>
+                <span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
+              </template>
+            </el-cascader>
+          </el-form-item>
+        </el-form>
+
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="userVisible = false">取 消</el-button>
+          <el-button type="primary" @click="confirmAssign">确 定</el-button>
+        </div>
+      </el-dialog>
+
+
     </div>
 </template>
 
 <script>
     export default {
-        name: "roles"
+      name: "roles",
+      data(){
+        return {
+          propsData: { multiple: true, expandTrigger: 'hover'},
+          roleData:[
+            {
+              roleName:'普通用户',
+              remarks:'查看个人资产，可操作盘点和退库'
+            },
+            {
+              roleName:'管理员',
+              remarks:'查看个人资产，可操作盘点和退库'
+            }
+          ],
+          multipleSelection:[],
+          createVisible:false,
+          createForm:{
+            roleName:'',
+            remarks:''
+          },
+          editVisible:false,
+          currentIndex:1,
+          roleAuth:[
+            {
+            id: 1,
+            label: '一级 1',
+            children: [{
+              id: 4,
+              label: '二级 1-1',
+              children: [{
+                id: 9,
+                label: '三级 1-1-1'
+              }, {
+                id: 10,
+                label: '三级 1-1-2'
+              }]
+            }]
+          },
+            {
+            id: 2,
+            label: '一级 2',
+            children: [{
+              id: 5,
+              label: '二级 2-1'
+            }, {
+              id: 6,
+              label: '二级 2-2'
+            }]
+          },
+            {
+            id: 3,
+            label: '一级 3',
+            children: [{
+              id: 7,
+              label: '二级 3-1'
+            }, {
+              id: 8,
+              label: '二级 3-2'
+            }]
+          }],
+          expandedKeys:[1,3],
+          checkedKeys:[7,9,5,6],
+          defaultProps: {
+            children: 'children',
+            label: 'label'
+          },
+          currentKey:'',
+          searchVal:'',
+          personData:[
+            {
+              id:1,
+              name:'张三',
+              blongDepart:'总部',
+              employee:'经理'
+            },
+            {
+              id:2,
+              name:'张三',
+              blongDepart:'总部',
+              employee:'经理'
+            },
+            {
+              id:3,
+              name:'张三',
+              blongDepart:'总部',
+              employee:'经理'
+            }
+          ],
+          personSelection:[],
+          authVisible:false,
+          authForm:{
+            roleName:''
+          },
+          userForm:{
+            roleName:'',
+            userList:[[1,2,3],[1,7,8]]
+          },
+          userVisible:false,
+          options: [
+            {
+              value: 1,
+              label: '东南',
+              children: [{
+                value: 2,
+                label: '上海',
+                children: [
+                  { value: 3, label: '普陀' },
+                  { value: 4, label: '黄埔' },
+                  { value: 5, label: '徐汇' }
+                ]
+              }, {
+                value: 7,
+                label: '江苏',
+                children: [
+                  { value: 8, label: '南京' },
+                  { value: 9, label: '苏州' },
+                  { value: 10, label: '无锡' }
+                ]
+              }, {
+                value: 12,
+                label: '浙江',
+                children: [
+                  { value: 13, label: '杭州' },
+                  { value: 14, label: '宁波' },
+                  { value: 15, label: '嘉兴' }
+                ]
+              }]
+            },
+            {
+              value: 17,
+              label: '西北',
+              children: [{
+                value: 18,
+                label: '陕西',
+                children: [
+                  { value: 19, label: '西安' },
+                  { value: 20, label: '延安' }
+                ]
+              }, {
+                value: 21,
+                label: '新疆维吾尔族自治区',
+                children: [
+                  { value: 22, label: '乌鲁木齐' },
+                  { value: 23, label: '克拉玛依' }
+                ]
+              }]
+            }]
+
+
+
+
+        }
+      },
+      methods:{
+        editRole(type){
+          if(type === 1){
+            this.createForm = this.$Store.resetForm(this.createForm);
+            this.createVisible = true;
+          }else if(type === 2){
+            if(this.multipleSelection.length === 1){
+              this.createForm = Object.assign({},this.multipleSelection[0]);
+              this.editVisible = true;
+            }else{
+              this.$message({
+                type:'warning',
+                message:'请选择一条要操作的数据'
+              })
+            }
+
+          }else if(type === 3){
+            if(this.multipleSelection.length >= 1){
+              this.$message({
+                type:'success',
+                message:'删除成功！'
+              })
+            }else{
+              this.$message({
+                type:'warning',
+                message:'请至少选择一条要删除的数据'
+              })
+            }
+
+          }
+
+        },
+        handleSelectionChange(val) {
+          this.multipleSelection = val;
+          console.log(this.multipleSelection);
+        },
+        handleSelection(val){
+          this.personSelection = val;
+        },
+        confirmCreate(){
+          this.createVisible = false;
+        },
+        confirmEdit(){
+          this.editVisible = false;
+        },
+        handleSelect(key) {
+          this.currentIndex = Number(key);
+          console.log(key);
+        },
+        checkedChange(data,status){
+          this.checkedKeys = status.checkedKeys;
+        },
+        addPerson(){
+
+        },
+        deletePerson(){
+
+        },
+        searchPerson(){
+
+        },
+        authRole(){
+          if(this.multipleSelection.length === 1){
+            this.authForm = Object.assign({},this.multipleSelection[0]);
+            this.authVisible = true;
+          }else{
+            this.$message({
+              type:'warning',
+              message:'请选择一个要授权的角色'
+            })
+          }
+
+        },
+        assignUser(){
+          if(this.multipleSelection.length === 1){
+            this.userForm = Object.assign({},this.userForm,this.multipleSelection[0]);
+            this.userVisible = true;
+          }else{
+            this.$message({
+              type:'warning',
+              message:'请选择一个要授权的角色'
+            })
+          }
+        },
+        confirmAuth(){
+          this.authVisible = false;
+        },
+        confirmAssign(){
+          this.userVisible = false;
+        }
+
+
+      },
+      watch:{
+        userForm:{
+          handler:function(val,oldval){
+            console.log(typeof val)
+            console.log(val)
+          },
+          deep:true
+        }
+      },
+      mounted(){
+        this.personData = new Array(20).fill(this.personData[0])
+      }
+
     }
 </script>
 
-<style scoped>
+<style lang="less">
+  #roles{
+    .roleTab{
+      margin-top: 20px;
+      tr td:nth-of-type(3) .cell{
+        color: #1bcaff;
+      }
+    }
+    .editDialog{
+      .el-dialog__body{
+        margin-top: -20px;
+        .el-menu{
+          margin-bottom: 20px;
+        }
+        .search{
+          width: 200px;
+          float: right;
+        }
+        .personZone{
+          margin-bottom: 20px;
+        }
+      }
+    }
+
+  }
+  .assign .el-input__inner{
+    width: 250px;
+  }
 
 </style>
